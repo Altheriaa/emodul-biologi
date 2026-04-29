27db845a34ec23cb8fb3631aba2b06129abe4779
Altheriaa <daffaalfharizy265@gmail.com>
2026-04-29 14:25:02 +0700

    1

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Hello dari Inertia + Vue 3!</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    message: String
+})
+</script>
